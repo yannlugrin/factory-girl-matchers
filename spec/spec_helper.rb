f972539d3cc7a7b959b8b20037b30ca5dccd5b32
Yann Lugrin <yann.lugrin@sans-savoir.net>
2009-03-18 12:11:57 +0100

    rubygems not required (http://gist.github.com/54177)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
+
 require 'spec'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'rubygems'
 require 'activerecord'
 require 'factory_girl'
 
